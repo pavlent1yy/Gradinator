77342b450d8f738d1398f7c44fc3dbd31392a2c6
Pavlent1yy <phuharev@gmail.com>
2026-09-21 21:14:09 +0300

    quickfix: вычисление типа недели, нормализация года отчета

--- g-api/src/main/java/com/pavlent1yy/gradinator/service/WeekService.java
+++ g-api/src/main/java/com/pavlent1yy/gradinator/service/WeekService.java
@@ -3,9 +3,11 @@ package com.pavlent1yy.gradinator.service;
 import com.pavlent1yy.gradinator.enums.WeekType;
 import org.springframework.stereotype.Service;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjusters;
 
 import static com.pavlent1yy.gradinator.enums.WeekType.DENOMINATOR;
 import static com.pavlent1yy.gradinator.enums.WeekType.NUMERATOR;
@@ -18,16 +20,22 @@ public class WeekService {
         return getWeekTypeByDate(today);
     }
 
-    public WeekType getWeekTypeByDate(LocalDate date){
-        int currentYear = date.getYear();
-        LocalDate startOfAcademicYear = LocalDate.of(currentYear, Month.SEPTEMBER, 1);
+    public WeekType getWeekTypeByDate(LocalDate date) {
+        int academicYear = date.getMonthValue() >= Month.SEPTEMBER.getValue()
+                ? date.getYear()
+                : date.getYear() - 1;
 
-        if (date.isBefore(startOfAcademicYear)) {
-            startOfAcademicYear = LocalDate.of(currentYear - 1, Month.SEPTEMBER, 1);
-        }
+        LocalDate septemberFirst = LocalDate.of(academicYear, Month.SEPTEMBER, 1);
 
-        long weeksBetween = ChronoUnit.WEEKS.between(startOfAcademicYear, date);
-        return weeksBetween % 2 == 0 ? DENOMINATOR : NUMERATOR;
+        LocalDate firstAcademicWeekStart = septemberFirst.with(
+                TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY)
+        );
+
+        long weeksBetween = ChronoUnit.WEEKS.between(firstAcademicWeekStart, date);
+
+        return weeksBetween % 2 == 0
+                ? DENOMINATOR
+                : NUMERATOR;
     }
 
 }
